7942e3b30d93dec87af98cf9b3d10906608af0ca
Dusan <dusanbojanic1@gmail.com>
2025-03-14 00:37:44 +0100

    added some backend functions and added schema models

--- src/random-functions/backend/backend1.ts
+++ src/random-functions/backend/backend1.ts
@@ -1,7 +1,74 @@
 import bcrypt from "bcrypt";
+import { db } from "@/server/db";
+import { election, match, player, vote } from "@/server/db/schema";
+import { eq, and } from "drizzle-orm";
+
+const policies = [
+  "liberal",
+  "liberal",
+  "liberal",
+  "liberal",
+  "liberal",
+  "liberal",
+
+  "fascist",
+  "fascist",
+  "fascist",
+  "fascist",
+  "fascist",
+  "fascist",
+
+  "fascist",
+  "fascist",
+  "fascist",
+  "fascist",
+  "fascist",
+];
+
+// Fascist track for 5-6 players
+export const fascistTrack_1 = [
+  { policy: 1, action: null },
+  { policy: 2, action: null },
+  { policy: 3, action: "PeekNextThreePolicies" },
+  { policy: 4, action: "Execution" },
+  { policy: 5, action: "Execution" },
+  { policy: 6, action: "Fascist Victory!" },
+];
+
+// Fascist track for 7-8 players
+export const fascistTrack_2 = [
+  { policy: 1, action: null },
+  { policy: 2, action: "InvestigatePlayer" },
+  { policy: 3, action: "SpecialElection" },
+  { policy: 4, action: "Execution" },
+  { policy: 5, action: "Execution" },
+  { policy: 6, action: "Fascist Victory!" },
+];
+
+// Fascist track for 9-10 players
+export const fascistTrack_3 = [
+  { policy: 1, action: null },
+  { policy: 2, action: "InvestigatePlayer" },
+  { policy: 3, action: "PolicyPeek" },
+  { policy: 4, action: "Execution" },
+  { policy: 5, action: "Execution" },
+  { policy: 6, action: "Fascist Victory!" },
+];
+
+export function shuffleArray<T>(array: T[]): T[] {
+  const shuffled = [...array]; // Create a copy to avoid mutating the original array
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1)); // Random index
+    // Swap only if j is a valid index within bounds
+    if (j !== undefined) {
+      [shuffled[i], shuffled[j]] = [shuffled[j]!, shuffled[i]!]; // Non-null assertion
+    }
+  }
+  return shuffled;
+}
 
 export async function hashPassword(password: string): Promise<string> {
-  const saltRounds = 10;
+  const saltRounds = Number(process.env.BCRYPT_SALT_ROUNDS);
   const salt = await bcrypt.genSalt(saltRounds);
   const hashedPassword = await bcrypt.hash(password, salt);
   return hashedPassword;
@@ -10,4 +77,293 @@ export async function sleep(ms: number): Promise<void> {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
+}
+
+function get_config_based_on_number_of_players(number_of_player: number) {
+  switch (number_of_player) {
+    case 5:
+      return {
+        fascist_track: fascistTrack_1,
+        fascist_numbers: 2,
+        hitler_has_intel: true,
+      };
+    case 6:
+      return {
+        fascist_track: fascistTrack_1,
+        fascist_numbers: 2,
+        hitler_has_intel: true,
+      };
+    case 7:
+      return {
+        fascist_track: fascistTrack_2,
+        fascist_numbers: 3,
+        hitler_has_intel: false,
+      };
+    case 8:
+      return {
+        fascist_track: fascistTrack_2,
+        fascist_numbers: 3,
+        hitler_has_intel: false,
+      };
+    case 9:
+      return {
+        fascist_track: fascistTrack_3,
+        fascist_numbers: 4,
+        hitler_has_intel: false,
+      };
+    default:
+      return {
+        fascist_track: fascistTrack_3,
+        fascist_numbers: 4,
+        hitler_has_intel: false,
+      };
+  }
+}
+
+export async function start_game(
+  match_id: string,
+  playerID: string,
+  name: string,
+) {
+  return await db.transaction(
+    async (tx) => {
+      // const locked_match = await tx
+      //   .select()
+      //   .from(match)
+      //   .where(eq(match.id, match_id))
+      //   .for("update");
+      const found_match = await tx.query.match.findFirst({
+        where: eq(match.id, match_id),
+        with: {
+          elections: {
+            with: {
+              votes: true, // Eager load votes
+            },
+          },
+          players: true,
+        },
+      });
+      if (!found_match) {
+        throw new Error("Match not found.");
+      }
+      if (found_match.has_started || found_match.isOver) {
+        throw new Error(
+          "It is impossible to join the match at this time, it has either already started and/or finished",
+        );
+      }
+      const config = get_config_based_on_number_of_players(
+        found_match.players.length,
+      );
+      const players = found_match.players;
+
+      // const new_players = players.sort(() => Math.random() - 0.5);
+
+      const new_players = shuffleArray(players);
+      const just_the_names: string[] = [];
+
+      if (!new_players.length || !new_players) {
+        throw new Error("No players found.");
+      }
+      for (let i = 0; i < new_players.length; i++) {
+        console.log(i);
+        just_the_names.push(new_players[i]!.username);
+      }
+
+      const new_just_the_names = shuffleArray(just_the_names);
+      const shuffled_policies = shuffleArray(policies);
+
+      for (let i = 0; i < config.fascist_numbers; i++) {
+        if (i === 0) {
+          await tx
+            .update(player)
+            .set({
+              is_fascist: true,
+              is_hitler: true,
+            })
+            .where(
+              and(
+                eq(player.id, new_players[i]!.id),
+                eq(player.username, new_players[i]!.username),
+                eq(player.match, new_players[i]!.match),
+              ),
+            );
+
+          await tx
+            .update(match)
+            .set({
+              president: new_just_the_names[0],
+              players_array: new_just_the_names,
+              hitler: new_players[i]!.username,
+              deck: shuffled_policies,
+            })
+            .where(eq(match.id, new_players[i]!.match));
+        } else {
+          await tx
+            .update(player)
+            .set({
+              is_fascist: true,
+            })
+            .where(
+              and(
+                eq(player.id, new_players[i]!.id),
+                eq(player.username, new_players[i]!.username),
+                eq(player.match, new_players[i]!.match),
+              ),
+            );
+        }
+      }
+    },
+    {
+      isolationLevel: "read committed",
+      accessMode: "read write",
+      deferrable: true,
+    },
+  );
+}
+export async function get_info_on_game(
+  match_id: string,
+  playerID: string,
+  name: string,
+) {
+  // const result = await db
+  //   .select()
+  //   .from(match)
+  //   .where(eq(match.id, "your-match-id"))
+  //   .leftJoin(player, eq(player.match, match.id)) // Join players
+  //   .leftJoin(election, eq(election.match, match.id)) // Join elections
+  //   .leftJoin(vote, eq(vote.election, election.id)) // Join votes (nested relation)
+  //   .for("update")
+  //   .limit(1); // Equivalent to findFirst()
+
+  const found_match = await db.query.match.findFirst({
+    where: eq(match.id, match_id),
+  });
+  return found_match;
+}
+
+export async function join_game(
+  match_id: string,
+  player_name: string,
+  password: string,
+) {
+  return await db.transaction(
+    async (tx) => {
+      const locked_match = await tx
+        .select()
+        .from(match)
+        .where(eq(match.id, match_id))
+        .for("update");
+
+      // Fetch the match with eager-loaded relations and lock it for update
+      const found_match = await tx.query.match.findFirst({
+        where: eq(match.id, match_id),
+        with: {
+          elections: {
+            with: {
+              votes: true, // Eager load votes
+            },
+          },
+          players: true,
+        },
+      });
+      if (!found_match) {
+        throw new Error("Match not found.");
+      }
+      if (found_match.has_started || found_match.isOver) {
+        throw new Error(
+          "It is impossible to join the match at this time, it has either already started and/or finished",
+        );
+      }
+
+      const players = found_match.players || [];
+      if (players.length >= 9) {
+        throw new Error("Match is at full capacity and cannot be joined");
+      }
+
+      const hashed_password = await hashPassword(password);
+
+      const new_player = await tx
+        .insert(player)
+        .values({
+          match: match_id,
+          username: player_name,
+          hashed_password: hashed_password,
+        })
+        .returning();
+
+      if (!new_player) {
+        throw new Error("couldn't create a new player");
+      }
+
+      const singular_new_player = new_player[0];
+
+      return {
+        found_match,
+        players,
+        singular_new_player,
+      };
+    },
+    {
+      isolationLevel: "read committed",
+      accessMode: "read write",
+      deferrable: true,
+    },
+  );
+}
+export async function create_game(
+  name: string,
+  username: string,
+  player_password: string,
+  liberal_faction_name: string,
+  fascist_faction_name: string,
+  president_role_name: string,
+  chancellor_role_name: string,
+  hitler_role_name: string,
+  liberal_faction_image_url: string,
+  fascist_faction_image_url: string,
+  president_role_image_url: string,
+  chancellor_role_image_url: string,
+  password: string,
+) {
+  return await db.transaction(
+    async (tx) => {
+      // Insert the new match and return it
+      const new_match_return = await tx
+        .insert(match)
+        .values({
+          name,
+          creator_owner: username,
+          password,
+          liberal_faction_name,
+          fascist_faction_name,
+          president_role_name,
+          chancellor_role_name,
+          hitler_role_name,
+          liberal_faction_image_url,
+          fascist_faction_image_url,
+          president_role_image_url,
+          chancellor_role_image_url,
+        })
+        .returning();
+
+      const new_match = new_match_return[0];
+
+      if (!new_match) {
+        throw new Error("Failed to create match");
+      }
+
+      const create_user_result = await join_game(
+        new_match.id,
+        username,
+        player_password,
+      );
+
+      return { new_match, create_user_result }; // Return the created match
+    },
+    {
+      isolationLevel: "read committed",
+      accessMode: "read write",
+      deferrable: true,
+    },
+  );
 }
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,15 +1,11 @@
 import { relations, sql } from "drizzle-orm";
 import {
-  index,
   integer,
   pgTableCreator,
-  primaryKey,
-  text,
   timestamp,
   varchar,
   boolean,
 } from "drizzle-orm/pg-core";
-import { type AdapterAccount } from "next-auth/adapters";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -25,7 +21,7 @@ export const cronjob_Runs = createTable("cronjob_Runs", {
   runDate: timestamp("runDate", { withTimezone: true }).primaryKey().notNull(),
 });
 
-export const match1 = createTable("match1", {
+export const match = createTable("match", {
   id: varchar("id", { length: 255 })
     .notNull()
     .primaryKey()
@@ -35,7 +31,7 @@ export const match1 = createTable("match1", {
     .notNull()
     .default(""),
   password: varchar("password", { length: 255 }),
-  all_questions: varchar("all_questions", { length: 3000 })
+  players_array: varchar("players", { length: 3000 })
     .array()
     .notNull()
     .default(sql`'{}'::text[]`),
@@ -47,17 +43,66 @@ export const match1 = createTable("match1", {
     .array()
     .notNull()
     .default(sql`'{}'::text[]`),
+
+  deck: varchar("deck", { length: 3000 })
+    .array()
+    .notNull()
+    .default(sql`'{}'::text[]`),
+
+  discard_pile: varchar("deck", { length: 3000 })
+    .array()
+    .notNull()
+    .default(sql`'{}'::text[]`),
+
   failed_elections: integer("failed_elections").notNull().default(0),
   liberal_laws: integer("liberal_laws").notNull().default(0),
   fascist_laws: integer("fascist_laws").notNull().default(0),
   president: varchar("president", { length: 2000 }).notNull().default(""),
   chancellor: varchar("chancellor", { length: 2000 }).notNull().default(""),
-  ElectionID: varchar("name_id", { length: 2000 }).references(
-    () => election.id,
-  ),
-  stage: varchar("stage", { length: 2000 }).notNull().default(""),
-  substage: varchar("substage", { length: 2000 }).notNull(),
-  hitler: varchar("hitler", { length: 2000 }).notNull(),
+  veto_power_unlocked: boolean("veto_power_unlocked").default(false).notNull(),
+  liberal_faction_name: varchar("liberal_faction_name", { length: 2000 })
+    .notNull()
+    .default("liberal"),
+  fascist_faction_name: varchar("fascist_faction_name", { length: 2000 })
+    .notNull()
+    .default("fascist"),
+  president_role_name: varchar("president_role_name", { length: 2000 })
+    .notNull()
+    .default("president"),
+  chancellor_role_name: varchar("chancellor_role_name", { length: 2000 })
+    .notNull()
+    .default("chancellor"),
+  hitler_role_name: varchar("hitler_role_name", { length: 2000 }).notNull(),
+
+  liberal_faction_image_url: varchar("liberal_faction_image_url", {
+    length: 2000,
+  })
+    .notNull()
+    .default("0"),
+  fascist_faction_image_url: varchar("fascist_faction_image_url", {
+    length: 2000,
+  })
+    .notNull()
+    .default("0"),
+  president_role_image_url: varchar("president_role_image_url", {
+    length: 2000,
+  })
+    .notNull()
+    .default(""),
+  chancellor_role_image_url: varchar("chancellor_role_image_url", {
+    length: 2000,
+  })
+    .notNull()
+    .default(""),
+  hitler_role_image_url: varchar("hitler_role_image_url", { length: 2000 }),
+
+  stage: varchar("stage", { length: 2000 }).notNull().default("lobby"),
+  substage: varchar("substage", { length: 2000 }).notNull().default("lobby"),
+  waiting_on: varchar("waiting_on", { length: 2000 }).notNull().default("All"),
+
+  hitler: varchar("hitler", { length: 2000 }).notNull().default(""),
+  isOver: boolean("isOver").notNull().default(false),
+  result: varchar("result", { length: 2000 }).default("TBA"),
   scheduled_for_deletion: boolean("scheduled_for_deletion").default(false),
   has_started: boolean("has_started").default(false),
 });
@@ -68,30 +113,33 @@ export const election = createTable("election", {
     .primaryKey()
     .$defaultFn(() => crypto.randomUUID()),
   name: varchar("name", { length: 2000 }).notNull(),
-  president: varchar("president", { length: 2000 }).notNull(),
-  chancellor: varchar("chancellor", { length: 2000 }).notNull(),
-
+  president_candidate: varchar("president", { length: 2000 }).notNull(),
+  chancellor_candidate: varchar("chancellor", { length: 2000 }).notNull(),
   votes: varchar("votes", { length: 2000 }).notNull(),
-  description: varchar("description", { length: 2000 }).notNull(),
-  author: varchar("author", { length: 255 })
+  votes_needed: integer("votes").notNull().default(0),
+  match: varchar("match", { length: 255 })
     .notNull()
-    .references(() => actual_users.username, {
+    .references(() => match.id, { onDelete: "cascade", onUpdate: "cascade" }),
+});
+
+export const vote = createTable("vote", {
+  id: varchar("id", { length: 255 })
+    .notNull()
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  username: varchar("username", { length: 255 })
+    .notNull()
+    .references(() => player.username, {
       onDelete: "cascade",
       onUpdate: "cascade",
     }),
-  createdById: varchar("createdById", { length: 255 })
+  voting_yes: boolean("voting_yes"),
+  election: varchar("election", { length: 255 })
     .notNull()
-    .references(() => actual_users.id, {
+    .references(() => election.id, {
       onDelete: "cascade",
       onUpdate: "cascade",
     }),
-  createdAt: timestamp("createdAt", { withTimezone: true })
-    .default(sql`CURRENT_TIMESTAMP`)
-    .notNull(),
-  updatedAt: timestamp("updatedAt", { withTimezone: true }).$onUpdate(
-    () => new Date(),
-  ),
-  isPublic: boolean("isPublic").default(false),
 });
 
 export const player = createTable("player", {
@@ -107,67 +155,22 @@ export const player = createTable("player", {
     }),
   hashed_password: varchar("hashed_password", { length: 255 }).notNull(),
   score: integer("score").default(0).notNull(),
-  answer: varchar("answer", { length: 255 }).default(""),
   match: varchar("match", { length: 255 })
     .notNull()
     .references(() => match.id, { onDelete: "cascade", onUpdate: "cascade" }),
+  is_fascist: boolean("is_fascist").notNull().default(false),
+  is_hitler: boolean("is_hitler").notNull().default(false),
+  party: varchar("party", { length: 255 }),
+  role: varchar("role", { length: 255 }),
 
-  liberals: varchar("all_player_ids", { length: 3000 })
+  intel: varchar("intel", { length: 3000 })
     .array()
     .notNull()
     .default(sql`'{}'::text[]`),
-});
 
-export const deck = createTable("deck", {
-  id: varchar("id", { length: 255 })
-    .notNull()
-    .primaryKey()
-    .$defaultFn(() => crypto.randomUUID()),
-  name: varchar("name", { length: 2000 }).notNull(),
-  description: varchar("description", { length: 2000 }).notNull(),
-  author: varchar("author", { length: 255 })
-    .notNull()
-    .references(() => actual_users.username, {
-      onDelete: "cascade",
-      onUpdate: "cascade",
-    }),
-  createdById: varchar("createdById", { length: 255 })
-    .notNull()
-    .references(() => actual_users.id, {
-      onDelete: "cascade",
-      onUpdate: "cascade",
-    }),
-  createdAt: timestamp("createdAt", { withTimezone: true })
+  joined_at: timestamp("joined_at", { withTimezone: true })
     .default(sql`CURRENT_TIMESTAMP`)
     .notNull(),
-  updatedAt: timestamp("updatedAt", { withTimezone: true }).$onUpdate(
-    () => new Date(),
-  ),
-  isPublic: boolean("isPublic").default(false),
-});
-
-export const question = createTable("question", {
-  id: varchar("id", { length: 255 })
-    .notNull()
-    .primaryKey()
-    .$defaultFn(() => crypto.randomUUID()),
-  text: varchar("text", { length: 1500 }),
-  isSituation: boolean("isSituation").notNull(),
-  createdById: varchar("createdById", { length: 255 })
-    .notNull()
-    .references(() => actual_users.id, {
-      onDelete: "cascade",
-      onUpdate: "cascade",
-    }),
-  deck: varchar("deck", { length: 255 })
-    .notNull()
-    .references(() => deck.id, { onDelete: "cascade", onUpdate: "cascade" }),
-  createdAt: timestamp("createdAt", { withTimezone: true })
-    .default(sql`CURRENT_TIMESTAMP`)
-    .notNull(),
-  updatedAt: timestamp("updatedAt", { withTimezone: true }).$onUpdate(
-    () => new Date(),
-  ),
 });
 
 export const actual_users = createTable("actual_users", {
@@ -182,31 +185,9 @@ export const actual_users = createTable("actual_users", {
   image: varchar("image", { length: 255 }),
 });
 
-export const match = createTable("match", {
-  id: varchar("id", { length: 255 })
-    .notNull()
-    .primaryKey()
-    .$defaultFn(() => crypto.randomUUID()),
-  name: varchar("name", { length: 255 }),
-  current_judge: varchar("current_judge", { length: 255 })
-    .notNull()
-    .default(""),
-  creator_owner: varchar("creator_owner", { length: 255 })
-    .notNull()
-    .default(""),
-  password: varchar("password", { length: 255 }),
-  all_questions: varchar("all_questions", { length: 3000 })
-    .array()
-    .notNull()
-    .default(sql`'{}'::text[]`),
-  question: varchar("question", { length: 3000 }).notNull(),
-  deck: varchar("deck", { length: 255 }).notNull(),
-  scheduled_for_deletion: boolean("scheduled_for_deletion").default(false),
-  has_started: boolean("has_started").default(false),
-});
-
 export const matchRelations = relations(match, ({ many }) => ({
   players: many(player),
+  elections: many(election),
 }));
 
 export const playerRelations = relations(player, ({ one }) => ({
@@ -215,128 +196,17 @@ export const playerRelations = relations(player, ({ one }) => ({
     references: [match.id],
   }),
 }));
-export const deckRelations = relations(deck, ({ many }) => ({
-  questions: many(question),
+export const electionRelations = relations(election, ({ one, many }) => ({
+  match: one(match, {
+    fields: [election.match],
+    references: [match.id],
+  }),
+  votes: many(vote),
 }));
 
-export const questionRelations = relations(question, ({ one }) => ({
-  deck: one(deck, {
-    fields: [question.deck],
-    references: [deck.id],
+export const voteRelations = relations(vote, ({ one }) => ({
+  election: one(election, {
+    fields: [vote.election],
+    references: [election.id],
   }),
 }));
-
-export type deck_type = typeof deck.$inferInsert;
-export type question_type = typeof question.$inferInsert;
-export type combined_type = deck_type & { questions: question_type[] };
-export type match_type = typeof match.$inferSelect;
-
-export const posts = createTable(
-  "post",
-  {
-    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
-    name: varchar("name", { length: 256 }),
-    createdById: varchar("created_by", { length: 255 })
-      .notNull()
-      .references(() => users.id),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date(),
-    ),
-  },
-  (example) => ({
-    createdByIdIdx: index("created_by_idx").on(example.createdById),
-    nameIndex: index("name_idx").on(example.name),
-  }),
-);
-
-export const users = createTable("user", {
-  id: varchar("id", { length: 255 })
-    .notNull()
-    .primaryKey()
-    .$defaultFn(() => crypto.randomUUID()),
-  name: varchar("name", { length: 255 }),
-  email: varchar("email", { length: 255 }).notNull(),
-  emailVerified: timestamp("email_verified", {
-    mode: "date",
-    withTimezone: true,
-  }).default(sql`CURRENT_TIMESTAMP`),
-  image: varchar("image", { length: 255 }),
-});
-
-export const usersRelations = relations(users, ({ many }) => ({
-  accounts: many(accounts),
-}));
-
-export const accounts = createTable(
-  "account",
-  {
-    userId: varchar("user_id", { length: 255 })
-      .notNull()
-      .references(() => users.id),
-    type: varchar("type", { length: 255 })
-      .$type<AdapterAccount["type"]>()
-      .notNull(),
-    provider: varchar("provider", { length: 255 }).notNull(),
-    providerAccountId: varchar("provider_account_id", {
-      length: 255,
-    }).notNull(),
-    refresh_token: text("refresh_token"),
-    access_token: text("access_token"),
-    expires_at: integer("expires_at"),
-    token_type: varchar("token_type", { length: 255 }),
-    scope: varchar("scope", { length: 255 }),
-    id_token: text("id_token"),
-    session_state: varchar("session_state", { length: 255 }),
-  },
-  (account) => ({
-    compoundKey: primaryKey({
-      columns: [account.provider, account.providerAccountId],
-    }),
-    userIdIdx: index("account_user_id_idx").on(account.userId),
-  }),
-);
-
-export const accountsRelations = relations(accounts, ({ one }) => ({
-  user: one(users, { fields: [accounts.userId], references: [users.id] }),
-}));
-
-export const sessions = createTable(
-  "session",
-  {
-    sessionToken: varchar("session_token", { length: 255 })
-      .notNull()
-      .primaryKey(),
-    userId: varchar("user_id", { length: 255 })
-      .notNull()
-      .references(() => users.id),
-    expires: timestamp("expires", {
-      mode: "date",
-      withTimezone: true,
-    }).notNull(),
-  },
-  (session) => ({
-    userIdIdx: index("session_user_id_idx").on(session.userId),
-  }),
-);
-
-export const sessionsRelations = relations(sessions, ({ one }) => ({
-  user: one(users, { fields: [sessions.userId], references: [users.id] }),
-}));
-
-export const verificationTokens = createTable(
-  "verification_token",
-  {
-    identifier: varchar("identifier", { length: 255 }).notNull(),
-    token: varchar("token", { length: 255 }).notNull(),
-    expires: timestamp("expires", {
-      mode: "date",
-      withTimezone: true,
-    }).notNull(),
-  },
-  (vt) => ({
-    compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
-  }),
-);
